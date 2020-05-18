944c9a13d0c603342c879af01fea4f1579c38e9c
arshunt234 <47279817+arshunt234@users.noreply.github.com>
2020-05-19 01:59:05 +0700

    Update json.php

--- json.php
+++ json.php
@@ -1,11 +1,3 @@
 <?php header("Content-Type: text/html"); ?>
-<?php header("Content-Type: application/json"); ?>
-    <form name="myform" action="https://explorer.earthengine.google.com/getProject" method="POST">
-      <input type="submit" value="Submit request" />
-    </form>
-
-
-
-<script type="text/javascript">
-    document.myform.submit();
-</script>
+<form name="myform" action="https://explorer.earthengine.google.com/getProject" method="POST"><input type="submit" value="Submit request" /></form>
+<script type="text/javascript">document.myform.submit();</script>
